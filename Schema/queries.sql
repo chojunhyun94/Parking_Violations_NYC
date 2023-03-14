d943f1e2164e7c6a649f6cc0ab9ccca0778185a5
Alex W <awarsha2@gmail.com>
2023-03-14 18:23:44 -0400

    changed some stuff to lower case for ease of use

--- Schema/queries.sql
+++ Schema/queries.sql
@@ -1,2 +1,6 @@
-SELECT * FROM "Parking_Violations_Clean";
+SELECT * FROM Parking_Violations_Clean;
 
+SELECT COUNT("clean.Violation_Code"), *
+FROM Parking_Violations_Clean as clean
+GROUP BY "clean.Violation_Code"
+ORDER BY count;
--- Schema/traffic_db.sql
+++ Schema/traffic_db.sql
@@ -5,6 +5,8 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 CREATE TABLE "Parking_Violations_Raw" (
     "Summons_Number" float   NOT NULL,
@@ -49,42 +51,42 @@ CREATE TABLE "Parking_Violations_Raw" (
     "Violation_Description" varchar(20)   NOT NULL
 );
 
-CREATE TABLE "Parking_Violations_Clean" (
-    "Registration_State" varchar(2)   NOT NULL,
-    "Plate_Type" varchar(3)   NOT NULL,
-    "Violation_Code" int   NOT NULL,
-    "Vehicle_Body_Type" varchar(4)   NOT NULL,
-    "Vehicle_Make" varchar(5)   NOT NULL,
-    "Violation_Time" timestamp   NOT NULL,
-    "Vehicle_Color" varchar(6)   NOT NULL,
-    "Vehicle_Year" int   NOT NULL
+CREATE TABLE "parking_violations_clean" (
+    "registration_state" varchar(2)   NOT NULL,
+    "plate_type" varchar(3)   NOT NULL,
+    "violation_code" int   NOT NULL,
+    "vehicle_body_type" varchar(4)   NOT NULL,
+    "vehicle_make" varchar(5)   NOT NULL,
+    "violation_time" varchar(5)   NOT NULL,
+    "vehicle_color" varchar(6)   NOT NULL,
+    "vehicle_year" int   NOT NULL
 );
 
 CREATE TABLE "Parking_Violations_ML" (
-    "Registration_State_Group" int   NOT NULL,
-    "Plate_Type_Group" int   NOT NULL,
-    "Violation_Code" int   NOT NULL,
-    "Vehicle_Body_Type_Group" int   NOT NULL,
-    "Vehicle_Make_Group" int   NOT NULL,
-    "Violation_Time" datetime   NOT NULL,
-    "Vehicle_Color_Group" int   NOT NULL,
-    "Vehicle_Year" int   NOT NULL
+    "registration_state_group" int   NOT NULL,
+    "plate_type_group" int   NOT NULL,
+    "violation_code" int   NOT NULL,
+    "vehicle_body_type_group" int   NOT NULL,
+    "vehicle_make_group" int   NOT NULL,
+    "violation_time" datetime   NOT NULL,
+    "vehicle_color_group" int   NOT NULL,
+    "vehicle_year" int   NOT NULL
 );
 
 CREATE TABLE "Parking_Violations_Board" (
-    "Registration_State" varchar(2)   NOT NULL,
-    "Plate_Type_Group_Name" varchar(3)   NOT NULL,
-    "Violation_Code" int   NOT NULL,
-    "Vehicle_Body_Type_Group_Name" varchar(4)   NOT NULL,
-    "Vehicle_Make_Group_Name" varchar(5)   NOT NULL,
-    "Vehicle_Expiration_Date" int   NOT NULL,
-    "Violation_Location" int   NOT NULL,
-    "Violation_Time" datetime   NOT NULL,
-    "Street_Name" varchar(20)   NOT NULL,
-    "Vehicle_Color_Group_Name" varchar(6)   NOT NULL,
-    "Vehicle_Year" int   NOT NULL
+    "registration_state" varchar(2)   NOT NULL,
+    "plate_type_group_name" varchar(3)   NOT NULL,
+    "violation_code" int   NOT NULL,
+    "vehicle_body_type_group_name" varchar(4)   NOT NULL,
+    "vehicle_make_group_name" varchar(5)   NOT NULL,
+    "vehicle_expiration_date" int   NOT NULL,
+    "violation_location" int   NOT NULL,
+    "violation_time" datetime   NOT NULL,
+    "street_name" varchar(20)   NOT NULL,
+    "vehicle_color_group_name" varchar(6)   NOT NULL,
+    "vehicle_year" int   NOT NULL
 );
 
 ALTER TABLE "Parking_Violations_Raw" ADD CONSTRAINT "fk_Parking_Violations_Raw_Registration_State_Plate_Type_Violation_Code_Vehicle_Body_Type_Vehicle_Make_Violation_Time_Vehicle_Color_Vehicle_Year" FOREIGN KEY("Registration_State", "Plate_Type", "Violation_Code", "Vehicle_Body_Type", "Vehicle_Make", "Violation_Time", "Vehicle_Color", "Vehicle_Year")
-REFERENCES "Parking_Violations_Clean" ("Registration_State", "Plate_Type", "Violation_Code", "Vehicle_Body_Type", "Vehicle_Make", "Violation_Time", "Vehicle_Color", "Vehicle_Year");
+REFERENCES "parking_violations_clean" ("registration_state", "plate_type", "violation_code", "vehicle_body_type", "vehicle_make", "violation_time", "vehicle_color", "vehicle_year");
 
