3d925494f106ca7b81a2648d91bd08fb254c3901
Alex W <awarsha2@gmail.com>
2023-03-14 20:06:32 -0400

    added queries

--- Schema/queries.sql
+++ Schema/queries.sql
@@ -1,6 +1,47 @@
 SELECT * FROM Parking_Violations_Clean;
 
-SELECT COUNT("clean.Violation_Code"), *
+SELECT COUNT(clean.violation_code), clean.registration_state, clean.plate_type, clean.violation_code, clean.vehicle_body_type, clean.vehicle_make, clean.vehicle_color, clean.vehicle_year
 FROM Parking_Violations_Clean as clean
-GROUP BY "clean.Violation_Code"
-ORDER BY count;
+GROUP BY clean.registration_state, clean.plate_type, clean.violation_code, clean.vehicle_body_type, clean.vehicle_make, clean.vehicle_color, clean.vehicle_year
+ORDER BY count DESC;
+
+SELECT COUNT(clean.registration_state), clean.registration_state
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.registration_state
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.registration_state
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.registration_state
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.plate_type
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.plate_type
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.vehicle_body_type
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.vehicle_body_type
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.vehicle_make
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.vahicle_make
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.vehicle_color
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.vehicle_color
+ORDER BY count DESC;
+
+SELECT COUNT(clean.violation_code), clean.violation_code, clean.vehicle_year
+FROM Parking_Violations_Clean as clean
+GROUP BY clean.violation_code, clean.vehicle_year
+ORDER BY count DESC;
+
